af8e3d8a0929206c94294392e52ebb4c2edaaeb6
ShubhamSharma2311 <sharma.2022ug2068@iiitranchi.ac.in>
2025-02-28 12:32:54 +0530

    commit after adding git ignore

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require("dotenv").config()
 
-const connection = mongoose.connect(process.env.MONGO_URL)
+const connection = mongoose.connect('mongodb+srv://admin:Shubham123@cluster0.ma6za.mongodb.net/')
 
 module.exports= {
     connection
